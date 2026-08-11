2ffd044c376ae2ac48b200294d5970c5d6302714
cwcarrillov <cwcarrillov@outlook.es>
2026-08-11 16:59:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
 import streamlit as st
 
 st.title("Bootcamp Data Analytics for Oil & Gas")
-
-pozo = "SPE-001"
-petroleo_bpd = 1250
-agua_bpd = 350.50
-status = True
-
-st.write(pozo)
-st.write(petroleo_bpd)
-st.write(agua_bpd)
-st.write(status)
-
 st.sidebar.title("Parámetros")
+
+modulos = st.sidebar.selectbox("Selecione un módulo", ["Introducción a variables", "Funciones"])
+
+if modulos == "Introducción a variables":
+
+  pozo = "SPE-001"
+  petroleo_bpd = 1250
+  agua_bpd = 350.50
+  status = True
+  
+  st.write(pozo)
+  st.write(petroleo_bpd)
+  st.write(agua_bpd)
+  st.write(status)
+elif  modulos == "Funciones": 
+  
+  def calcular_caudal_vogel(caudal_maximo=1000, presion_yacimiento=3000, presion_fondo=200, decimales=2):
+  """
+    Calcula el caudal de petróleo mediante la ecuación de Vogel.
+
+    Parámetros:
+    caudal_maximo (float): Caudal máximo teórico del pozo, BPD.
+    presion_yacimiento (float): Presión promedio del yacimiento, psi.
+    presion_fondo (float): Presión de fondo fluyente, psi.
+    decimales (int): Número de decimales del resultado.
+
+    Retorna:
+    float: Caudal estimado de petróleo, BPD.  
+  """
+
+  relacion_presion = presion_fondo/presion_yacimiento
+  caudal = caudal_maximo*( 1 -0.2*relacion_presion - 0.8*(relacion_presion**2))
+  
+  return round(caudal, decimales)
+
+  caudal_maximo = st.number_input("Ingrese el caudal máximo")
+
+
+
